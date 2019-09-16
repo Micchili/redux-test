698d0f68c651a8207d3aa5812c4147f46f096603
Miccili <wot.dannbo@gmail.com>
2019-09-17 04:25:03 +0900

    [FIX]payload削除

--- src/booleanIsOpen.tsx
+++ src/booleanIsOpen.tsx
@@ -8,40 +8,28 @@ export const CLOSE = "CLOSE" as const;
 // interfaceでOpenActionを定義
 interface OpenAction extends Action {
   type: typeof OPEN
-  payload: Drower
 }
 
 // 引数はクリックで発火するだけなので無し、戻り値をOpenActionで定義
 export const open = (): OpenAction => ({
-  type: OPEN,
-  payload: {
-    isOpen: true
-  }
+  type: OPEN
 })
 
 // interfaceでCloseActionを定義
 interface CloseAction extends Action {
   type: typeof CLOSE
-  payload: Drower
 }
 
 // 戻り値をCloseActionで定義
 export const close = (): CloseAction => ({
-  type: CLOSE,
-  payload: {
-    isOpen: false
-  }
+  type: CLOSE
 })
 
 // Actionの型をまとめた、DrowerActions型をエクスポート
 // 複数ある場合、union型で繋ぐ
 export type DrowerActions = OpenAction | CloseAction;
 
-//////////////////
-// Reducer
-//////////////////
-
-// initialStateには、DrowerState型をあてはめる
+// initialStateには、Drower型をあてはめる
 const initialState: Drower = {
   isOpen: false
 }
